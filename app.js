b11c87637434b2bfc6a7d1581b781fd23a88c947
Raihan hosen <78276397+raihanhosen01@users.noreply.github.com>
2021-04-28 15:12:01 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// DOM elements
+const chackBoxs = document.querySelectorAll('.progress-form-wrapper input[type="checkbox"]')
+const progressLabel = document.querySelector('.progress-label')
+
+//Progress label counter
+var counter = 0
+
+//chack box dynamic counter 
+chackBoxs.forEach((element) => 
+  element.addEventListener("click", function (e) {
+    const val = Math.floor(e.target.value);
+    if (element.checked) counter += val
+    else counter -= val
+    progressLabel.style.width = counter + "%"
+  })
+)
